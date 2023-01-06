62d079b5c5f2649adf1fe7100e7ffd3ef7d35c75
tekitouk <47982907+koduma@users.noreply.github.com>
2023-01-06 15:19:59 +0900

    Update poker.cpp

--- poker.cpp
+++ poker.cpp
@@ -24,6 +24,16 @@
 
 using namespace std;
 
+double pro[9]={17.4,61.2,84.7,89.5,94.1,97.1,99.7,99.87,99.901};
+int pattern[9]={23294460,58627800,31433400,6461620,6180020,4047644,3473184,224848,41584};
+string type[9]={"HIGH_CARD","ONE_PAIR","TWO_PAIR","THREE_CARD","STRAIGHT","FLUSH","FULL_HOUSE","FOUR_CARD","STRAIGHT_FLUSH"};
+double prev_looked[9]={153,332,189,40,25,18,17,1,1};
+double sum_bet[9]={53177,268171,256026,53878,37735,16679,26441,3000,5000};
+double looked[9];
+double bed[9]={53177,268171,256026,53878,37735,16679,26441,3000,5000};
+double led[9]={153,332,189,40,25,18,17,1,1};
+//double led[9]={62,400,394,31,17,351,110,113,251};
+
 int rnd(int mini, int maxi) {
 	static mt19937 mt((int)time(0));
 	uniform_int_distribution<int> dice(mini, maxi);
@@ -36,58 +46,39 @@ double d_rnd() {
 	return dice(mt)*100.0;
 }
 
-double pro[9]={17.4,61.2,84.7,89.5,94.1,97.1,99.7,99.87,99.901};
-int pattern[9]={23294460,58627800,31433400,6461620,6180020,4047644,3473184,224848,41584};
-string type[9]={"HIGH_CARD","ONE_PAIR","TWO_PAIR","THREE_CARD","STRAIGHT","FLUSH","FULL_HOUSE","FOUR_CARD","STRAIGHT_FLUSH"};
-
-double sum_bet[9]={53177,268171,256026,53878,37735,16679,26441,3000,5000};
-double looked[9]={153,332,189,40,25,18,17,1,1};
-
-int main(){
-    
-    
-int win=0;
-int lose=0;
-
-for(int i=0;i=10000;i++){
-
-int me=20000;
-int cpu=20000;
-int sb=100;
-int bb=200;
+struct gamestate{
+int mychip;
+int enemychip;
+int sb;
+int bb;
 int D;
-
-cout<<"sum_bet=";
-for(int k=0;k<9;k++){cout<<sum_bet[k]<<",";}
-cout<<endl;
-    
-cout<<"looked=";
-for(int k=0;k<9;k++){cout<<looked[k]<<",";}
-cout<<endl;
-
-cout<<"win="<<win<<",lose="<<lose<<endl;
-    
-for(int ddd=0;;ddd++){
-    
-D=ddd%2;
-
-if(me<=0){   
-cout<<"you lose"<<endl;
-break;
-}
-if(cpu<=0){
-cout<<"you win"<<endl;
-break;
-}
-//deal_round 
-    
-double deal1=d_rnd();
-double deal2=d_rnd();
-int mycard=0;
-int cpucard=0;
+int pot;
+int mybet;
+int enemybet;
+int mycard;
+int cpucard;
 double myrank;
 double cpurank;
+bool myallin;
+bool enemyallin;
+int fight;
+    
+void init(){
+mychip=20000;
+enemychip=20000;
+D=0;    
+}    
 
+void deal(){
+sb=100;
+bb=200;
+pot=0;    
+mybet=0;
+enemybet=0;
+double deal1=d_rnd();
+double deal2=d_rnd();
+    
+mycard=0;    
 for(int j=0;j<8;j++){
 if(pro[j]<deal1&&deal1<=pro[j+1]){
 mycard=j+1;
@@ -95,6 +86,7 @@ break;
 }
 }
 
+cpucard=0;    
 for(int j=0;j<8;j++){
 if(pro[j]<deal2&&deal2<=pro[j+1]){
 cpucard=j+1;
@@ -103,323 +95,427 @@ break;
 }
 
 myrank=(double)(pattern[mycard]-rnd(1,pattern[mycard]))*100.0/(double)pattern[mycard];
-cpurank=(double)(pattern[cpucard]-rnd(1,pattern[cpucard]))*100.0/(double)pattern[cpucard];   
+cpurank=(double)(pattern[cpucard]-rnd(1,pattern[cpucard]))*100.0/(double)pattern[cpucard];       
+myallin=false;
+enemyallin=false;
+fight=0;    
+
+if(D==0){
+mychip-=sb;
+enemychip-=bb;
+mybet=sb;
+enemybet=bb;    
+}
+else if(D==1){
+mychip-=bb;
+enemychip-=sb;
+mybet=bb;
+enemybet=sb;    
+}
+
+pot=sb+bb;    
     
-looked[mycard]+=1.0;
+}
+
+bool showdown(){
+if(mycard>cpucard){
+mychip+=pot;
+}
+else if(mycard==cpucard){
+if(myrank<cpurank){mychip+=pot;}
+else if(myrank==cpurank){mychip+=pot/2;enemychip+=pot/2;}
+else{enemychip+=pot;}
+}
+else{
+enemychip+=pot;
+}
+return true;    
+}    
     
-//bet_round
+bool judge(){
+if(enemychip<=0){return true;}
+if(mychip<=0){return true;}
+return false;    
+}    
     
-int pot=sb+bb;
-int bet=0;
+bool Ibetting(int bet){
+    if(mychip+mybet<bet){return false;}
+    if(enemybet>bet){return false;}
+    if(mybet>bet){return false;}
+    mychip-=bet-mybet;
+    pot+=bet-mybet;
+    mybet=bet;
+    D++;
+    D%=2;
+    if(mychip==0){
+    myallin=true;
+    }
+    return true;
+}
+bool enemybetting(int bet){
+    if(enemychip+enemybet<bet){return false;}
+    if(mybet>bet){return false;}
+    if(enemybet>bet){return false;}
+    enemychip-=bet-enemybet;
+    pot+=bet-enemybet;
+    enemybet=bet;
+    D++;
+    D%=2;
+    if(enemychip==0){
+    enemyallin=true;
+    }
+    return true;
+}
+bool Icheck(){
+if(mybet==0&&enemybet==0){
+    D++;
+    D%=2;
+    return true;
+}
+return false;    
+}    
+bool enemycheck(){
+if(mybet==0&&enemybet==0){
+    D++;
+    D%=2;
+    return true;
+}
+return false;    
+}
     
-bool me_allin=false;
-bool cpu_allin=false;
-bool breek=false;
+bool Icall(){
+    if(mybet>enemybet){return false;}
+    if(mychip+mybet<=enemybet){
+    pot+=mychip;    
+    mybet+=mychip;
+    pot-=enemybet-mybet;
+    enemychip+=enemybet-mybet;    
+    mychip=0;        
+    }
+    else{
+    mychip-=enemybet-mybet;
+    pot+=enemybet-mybet;
+    mybet=enemybet;
+    }    
+    D++;
+    D%=2;
+    return true;
+}    
+bool enemycall(){
+    if(enemybet>mybet){return false;}
+    if(enemychip+enemybet<=mybet){
+    pot+=enemychip;
+    enemybet+=enemychip;
+    pot-=mybet-enemybet;
+    mychip+=mybet-enemybet;    
+    enemychip=0;
+    }
+    else{
+    enemychip-=mybet-enemybet;
+    pot+=mybet-enemybet;
+    enemybet=mybet;
+    }    
+    D++;
+    D%=2;
+    return true;
+}    
+bool Ifold(){
+D++;
+D%=2;    
+enemychip+=pot;
+pot=0;
+mybet=0;
+enemybet=0;
+return true;
+}    
+bool enemyfold(){
+D++;
+D%=2;    
+mychip+=pot;
+pot=0;
+mybet=0;
+enemybet=0;
+return true;
+}     
     
-for(int t=0;;t++){
     
-    if(breek){break;}   
+};
+
+struct game{    
+//double enemy_sum_bet[9]={16000,70000,26000,10000,30374,4000,5000,5000,5000};
+//double enemy_looked[9]={8,36,14,5,7,2,1,1,1};
+double enemy_sum_bet[9]={0};
+double enemy_looked[9]={0};    
     
-        if(t==0&&D==0){
-        me-=sb;
-        cpu-=bb;
-        }
-        else if(t==0&&D==1){
-        me-=bb;
-        cpu-=sb;
-        }
+void battle(){     
     
-        if(!me_allin&&!cpu_allin){
+int win=0;
+int lose=0;
+
+gamestate gs;
     
-        if(me<=0||cpu<=0){
-        if(mycard>cpucard){
-            me+=pot;
-        }
-        else if(mycard==cpucard){
-        if(myrank<cpurank){me+=pot;}
-        else if(myrank==cpurank){me+=pot/2;cpu+=pot/2;}
-        else{cpu+=pot;}
-        }
-        else{
-        cpu+=pot;
-        }
-        pot=0;
-        bet=0;
-        if(me<=0){lose++;breek=true;}
-        if(cpu<=0){win++;breek=true;}
-        cout<<"my_card=("<<type[mycard]<<",top"<<myrank<<"%)"<<endl;
-        cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-        break;
-        }
-        }
-	
-	cout<<"-------------------"<<endl;
-	cout<<"mychip="<<me<<endl;
-	cout<<"cpuchip="<<cpu<<endl;
-	cout<<"my_card=("<<type[mycard]<<",top"<<myrank<<"%)"<<endl;
-	cout<<"pot="<<pot<<endl;
-	cout<<"bet="<<bet<<endl;
-	cout<<"-------------------"<<endl;
+gs.init();    
     
-        if(D==0){
-        cout<<"me_round:"<<t+1<<endl;
-            
-        cout<<"action(check/bet/call/fold)?=";    
-            
-        string ans="";
-        cin>>ans;
-            
-        while(1){
-            
-        if(bet==0&&ans=="check"){break;}
-        if((ans=="bet"&&me>=bet&&cpu_allin==false)||(ans=="call"&&bet>0)||ans=="fold"){break;}
-        cin>>ans; 
-        }   
-        //int pattern[9]={23294460,58627800,31433400,6461620,6180020,4047644,3473184,224848,41584};    
-        if(ans=="check"){D++;D%=2;}
-        else if(ans=="bet"){
-        while(1){
-        cout<<"bet_size("<<bet<<"~"<<min(me,cpu+bet)<<")?=";
-        int tmp;
-        cin>>tmp;
-        if(bet<=tmp&&tmp<me&&tmp<=cpu+bet){bet=tmp;break;}
-        if(tmp==me){
-            if(bet<tmp){bet=tmp;}
-            break;
-        }
-        }
-        me-=bet;
-        pot+=bet;
-        sum_bet[mycard]+=(double)bet;
-        if(me==0){me_allin=true;}
-        else{me_allin=false;}
-        D++;D%=2;
-        }
-        else if(ans=="call"){
-            if(cpu_allin==true){
-            
-            if(me>=bet){//me(call,10000) vs cpu(allin,2300)
-            me-=bet;pot+=bet;bet=bet;
-            }
-            else{//me(call,2300) vs cpu(allin,10000)
-            cpu+=bet-me;//10000-2300
-            pot+=me;//2300
-            pot-=bet-me;
-            bet=me;
-            me=0;
-            }
-            
-            }
-            else{
-            if(me<=bet){//me(call,2300) vs cpu(bet,10000)
-                cpu+=bet-me;
-                pot+=me;
-                pot-=bet-me;
-                bet=me;
-                me=0;
-            }//allin
-            else{
-            me-=bet;pot+=bet;bet=bet;
-            }
-            }
-            
-            sum_bet[mycard]+=(double)bet;
-            
-            if(mycard>cpucard){
-            me+=pot;
-            }
-            else if(mycard==cpucard){
-            if(myrank<cpurank){me+=pot;}
-            else if(myrank==cpurank){me+=pot/2;cpu+=pot/2;}
-            else{cpu+=pot;}
-            }
-            else{
-            cpu+=pot;
-            }
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}  
-            break;
-        }
-        else if(ans=="fold"){
-            D++;
-            D%=2;
-            cpu+=pot;
-            pot=0;
-            bet=0;
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-        }
-        }//D=0
-        else{
-        cout<<"cpu_round:"<<t+1<<endl;
-            
-        cout<<"action=";
-            
-        double div=1.0;
-        for(int h=0;h<9;h++){
-        div+=sum_bet[h]/(looked[h]+1.0);
-        }
-        double ai=(sum_bet[cpucard]/(looked[cpucard]+1.0))/div;
-        int cpubet=(int)floor((double)cpu*ai*(0.5+(d_rnd()/100.0))/2.0);
-        if(cpucard>=2){
-        if(cpucard==2){
-            if(rnd(1,100)<=10){
-            cpubet=cpu;
-            }
-        }
-        else{
-        cpubet=cpu;
-        }
-        }
-        if(cpubet>cpu){
-        cpubet=cpu;
-        }    
-        if(cpubet==cpu&&cpubet>0){
-            cout<<"cpu=allin="<<cpubet<<endl;
-            if(me_allin==true){
-                
-            if(bet>cpubet){//me(allin,10000) vs cpu(allin,4000)
-            me+=bet-cpubet;//10000-4000
-            pot+=cpubet;//4000
-            pot-=bet-cpubet;//-(10000-4000)
-            cpu-=cpubet;bet=cpubet;
-            }
-            else if(bet<cpubet){//me(allin,4000) vs cpu(allin,10000)
-            cpu-=bet;pot+=bet;bet=bet;
-            }
-            else{
-            pot+=bet;cpu-=bet;bet=bet;
-            }
-                
-            if(mycard>cpucard){
-            me+=pot;
-            }
-            else if(mycard==cpucard){
-            if(myrank<cpurank){me+=pot;}
-            else if(myrank==cpurank){me+=pot/2;cpu+=pot/2;}
-            else{cpu+=pot;}
-            }
-            else{
-            cpu+=pot;
-            }
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-            }
-            else{
-                
-            if(bet<cpubet){//me(bet,5000) vs cpu(allin,10000)
-            pot+=cpubet;cpu-=cpubet;bet=cpubet;
-            }
-            else{
-                
-            if(bet>cpubet){//me(bet,5200) vs cpu(allin,2000)
-            me+=bet-cpubet;
-            pot+=cpubet;
-            pot-=bet-cpubet;
-            cpu-=cpubet;bet=cpubet;
-            }
-            else{
-            pot+=bet;cpu-=bet;bet=bet;
-            }
-                
-            if(mycard>cpucard){
-            me+=pot;
-            }
-            else if(mycard==cpucard){
-            if(myrank<cpurank){me+=pot;}
-            else if(myrank==cpurank){me+=pot/2;cpu+=pot/2;}
-            else{cpu+=pot;}
-            }
-            else{
-            cpu+=pot;
-            }
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl; 
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-            
-            }
-            if(cpu==0){
-            cpu_allin=true;
-            }
-            else{
-            cpu_allin=false;
-            }
-            D++;D%=2;
-            }
-        }
-        else if(cpubet==0){
-            cout<<"cpu=fold"<<endl;
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            me+=pot;
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-        }
-        else{
-            if(cpubet>bet){
-            cout<<"cpu=bet="<<cpubet<<endl;
-            pot+=cpubet;cpu-=cpubet;bet=cpubet;
-            D++;D%=2;
-            }
-            else if(cpubet==bet){
-            cout<<"cpu=call"<<endl;
-            pot+=cpubet;cpu-=cpubet;bet=cpubet;
-                
-            if(mycard>cpucard){
-            me+=pot;
-            }
-            else if(mycard==cpucard){
-            if(myrank<cpurank){me+=pot;}
-            else if(myrank==cpurank){me+=pot/2;cpu+=pot/2;}
-            else{cpu+=pot;}
-            }
-            else{
-            cpu+=pot;
-            }
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-                
-            }
-            else{
-            cout<<"cpu=fold"<<endl;
-            cout<<"cpu_card=("<<type[cpucard]<<",top"<<cpurank<<"%)"<<endl;
-            me+=pot;
-            D++;D%=2;
-            pot=0;
-            bet=0;
-            if(me<=0){lose++;breek=true;}
-            if(cpu<=0){win++;breek=true;}
-            break;
-            }
-        }   
-     }
+for(int i=0;i=100;i++){    
+    
+while(1){    
+gs.deal();
+enemy_looked[gs.mycard]++;
+if(gs.mychip<=0){lose++;cout<<"youlose"<<endl;return;}
+if(gs.enemychip<=0){win++;cout<<"youwin"<<endl;return;}     
+    
+while(1){    
+
+cout<<"round:"<<gs.fight+1<<endl;
+    
+cout<<"-------------------"<<endl;
+cout<<"mychip="<<gs.mychip<<endl;
+cout<<"cpuchip="<<gs.enemychip<<endl;
+cout<<"my_card=("<<type[gs.mycard]<<",top"<<gs.myrank<<"%)"<<endl;
+cout<<"pot="<<gs.pot<<endl;
+cout<<"bet="<<gs.enemybet<<endl;    
+cout<<"-------------------"<<endl;    
+    
+if(gs.D==0){
+cout<<"action(bet/call/fold)?=";    
+string ans="";
+cin>>ans;
+while(1){
+/*    
+if(ans=="check"){
+    if(gs.Icheck()){
+    break;
+    }    
+}
+*/
+if(ans=="bet"){
+if(gs.mychip>=gs.enemybet&&(!gs.enemyallin)){break;}
+}
+if(ans=="call"){
+if(gs.Icall()){break;}
+}
+if(ans=="fold"){
+if(gs.Ifold()){break;}
+}
+cin>>ans;  
+}
+if(ans=="bet"){
+while(1){
+cout<<"bet_size("<<gs.enemybet<<"~"<<min(gs.mychip+gs.mybet,gs.enemychip+gs.enemybet)<<")?=";
+int tmp;
+cin>>tmp;
+if(gs.enemybet<=tmp&&tmp<=min(gs.mychip+gs.mybet,gs.enemychip+gs.enemybet)){
+    gs.Ibetting(tmp);
+    enemy_sum_bet[gs.mycard]+=(double)tmp;
+    if(tmp==gs.enemybet){
+    gs.showdown();
+    if(gs.mychip<=0){lose++;cout<<"youlose"<<endl;return;}
+    if(gs.enemychip<=0){win++;cout<<"youwin"<<endl;return;}    
+    }
+    break;
+} 
+}
+}
+else if(ans=="call"){
+enemy_sum_bet[gs.mycard]+=(double)gs.mybet;
+cout<<"cpu_card=("<<type[gs.cpucard]<<",top"<<gs.cpurank<<"%)"<<endl;
+gs.showdown();
+if(gs.mychip<=0){lose++;cout<<"youlose"<<endl;return;}
+if(gs.enemychip<=0){win++;cout<<"youwin"<<endl;return;}     
+break;    
+}
+else if(ans=="fold"){
+cout<<"cpu_card=("<<type[gs.cpucard]<<",top"<<gs.cpurank<<"%)"<<endl;    
+break;
+}    
+}
+else{            
+double div=1.0;
+for(int h=0;h<9;h++){
+div+=bed[h]/(led[h]+1.0);
+}
+int money=gs.enemychip+gs.enemybet;  
+double ai=(bed[gs.cpucard]/(led[gs.cpucard]+1.0))/div;
+int cpubet=(int)floor((double)money*ai*(0.5+(d_rnd()/100.0))/2.0);
+if(gs.cpucard>=2){
+if(gs.cpucard==2){
+if(rnd(1,100)<=10){
+cpubet=money;
+}
+}
+else{
+cpubet=money;
+}
+}
+if(cpubet>money){
+cpubet=money;
+}
+    
+int act=0;    
+    
+bool ret=gs.enemybetting(cpubet);
+if(!ret){
+ret=gs.enemyfold();
+act=1;    
+}
+    
+if(act==0){
+cout<<"cpu=action=bet="<<cpubet<<endl;
+}
+else if(act==1){
+cout<<"cpu=action=fold"<<endl;
+cout<<"cpu_card=("<<type[gs.cpucard]<<",top"<<gs.cpurank<<"%)"<<endl;   
+break;    
+}   
+    
+}
+
+gs.fight++;    
+}
+}
+}
+}    
+    
+int self_battle(){    
+    
+gamestate gs;
+    
+gs.init();    
+    
+for(int i=0;i=100;i++){  
+    
+while(1){    
+gs.deal();
+if(gs.mychip<=0){return 0;}
+if(gs.enemychip<=0){return 1;}    
+    
+while(1){    
+
+if(gs.D==0){
+double div=1.0;
+for(int h=0;h<9;h++){
+div+=sum_bet[h]/(looked[h]+1.0);
+}
+int money=gs.mychip+gs.mybet;    
+double ai=(sum_bet[gs.mycard]/(looked[gs.mycard]+1.0))/div;
+int mbet=(int)floor((double)money*ai*(0.5+(d_rnd()/100.0))/2.0);
+if(gs.mycard>=2){
+if(gs.mycard==2){
+if(rnd(1,100)<=10){
+mbet=money;
+}
+}
+else{
+mbet=money;
+}
+}
+if(mbet>money){
+mbet=money;
+}
+    
+bool ret=gs.Ibetting(mbet);
+if(!ret){
+ret=gs.Ifold();
+break;    
+}
+else{
+if(mbet==gs.enemybet){
+gs.showdown();
+if(gs.mychip<=0){return 0;}
+if(gs.enemychip<=0){return 1;} 
 }
 }    
 }
-
-
-return 0;
+else{            
+double div=1.0;
+for(int h=0;h<9;h++){
+div+=enemy_sum_bet[h]/(enemy_looked[h]+1.0);
 }
+int money=gs.enemychip+gs.enemybet;    
+double ai=(enemy_sum_bet[gs.cpucard]/(enemy_looked[gs.cpucard]+1.0))/div;
+int cpubet=(int)floor((double)money*ai*(0.5+(d_rnd()/100.0))/2.0);
+if(gs.cpucard>=2){
+if(gs.cpucard==2){
+if(rnd(1,100)<=10){
+cpubet=money;
+}
+}
+else{
+cpubet=money;
+}
+}
+if(cpubet>money){
+cpubet=money;
+}
+    
+bool ret=gs.enemybetting(cpubet);
+if(!ret){
+ret=gs.enemyfold();
+break;    
+}
+else{
+if(cpubet==gs.mybet){    
+gs.showdown();
+if(gs.mychip<=0){return 0;}
+if(gs.enemychip<=0){return 1;}    
+}    
+}    
+    
+}
+
+gs.fight++;    
+}
+}
+} 
+
+return 0;    
+}   
+    
+};
+
+
+int main(){
+    
+    game g;
+    
+    g.battle();
+    
+    cout<<"sum_bet=";
+    for(int k=0;k<9;k++){cout<<g.enemy_sum_bet[k]<<",";}
+    cout<<endl;
+    
+    cout<<"looked=";
+    for(int k=0;k<9;k++){cout<<g.enemy_looked[k]<<",";}
+    cout<<endl;
+    
+    for(int i=0;i<100000;i++){    
+    int win=0;
+    if((i+1)%1000==0){    
+    printf("train=%d/%d\n",i+1,100000);
+    }
+    memcpy(looked,prev_looked,sizeof(looked));
+    for(int j=0;j<9;j++){
+    int low=0;
+    if(looked[j]<0.1){low=0;}
+    else{low=-1;}
+    looked[j]+=(double)rnd(low,1);
+    }    
+    for(int j=0;j<100;j++){    
+    win+=g.self_battle();
+    }
+    if(win>50){    
+    memcpy(prev_looked,looked,sizeof(looked));
+    }    
+    }
+    
+    cout<<"looked=";
+    for(int k=0;k<9;k++){cout<<prev_looked[k]<<",";}
+    cout<<endl;
+    
+    memcpy(led,prev_looked,sizeof(led));
+    
+    g.battle();
+
+
+    return 0;
+};
